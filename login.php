6c60786f6b8e44854f65d1a07e39decacf616ac5
slackjeff <lrcjefferson@gmail.com>
2019-08-10 20:59:24 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -13,102 +13,28 @@
         <!-- LINKS -->
         <?php require_once 'html/nav.php' ?>
 
-        <!-- Informação curta da Localização do evento -->
         <div id="container">
-            <h1>Conferência Anual da comunidade Slackware</h1>
-            <div class="local">
-                <ul>
-                    <h2>> SlackCON v0</h2>
-                    <b>> Local:</b> Blumenau - Santa Catarina - Brasil
+            <h1>Login SlackCON</h1>
+            <form action="action_page.php">
+                <div class="container">
+                    <label for="uname"><b>Username</b></label>
+		    <input type="text" placeholder="Enter Username" name="uname" required>
+
+		    <label for="psw"><b>Password</b></label>
+		    <input type="password" placeholder="Enter Password" name="psw" required>
+
+		    <button type="submit">Login</button>
+		    <button type="button" class="cancelbtn">Cancel</button>
+		  </div>
+
+		  <div class="containerformclass" style="background-color:#f1f1f1">
+		    <span class="psw">Esqueceu a <a href="#">senha?</a></span>
                     <br>
-                    <b>> Data:</b> 23, 24, 25 e 26 de Novembro de 2020
-                    <br>
-                    <b>> Localização:</b>
-                    <a href="#" target="_blank">OpenStreet</a>,
-                    <a href="#" target="_blank">Google Maps</a>
-                    <br>
-                    <br>
-                    <a href="img/blumenau.jpg"><img class="imglocal" src="img/blumenau.jpg" alt="Foto do Centro da cidade"></a><br>
-                    <br>
-                    <a href="versoes/0/index.html">Saiba Mais</a>
-                </ul>
-            </div>
-            <br>
-            <!-- CONTÉUDO -->	
-            <h2>O que é SlackCON?</h2>
-            <p>
-                SlackCON é um esforço da comunidade para organizar um evento anual de quatro dias, direcionada a distribuição Slackware Linux, a distribuição Linux mais antiga ainda em desenvolvimento.
-            </p>
+                    <span class="psw">Registre-se <a href="#">agora</a></span>
+		  </div>
+            </form>
 
-            <p>
-                O evento é um verdadeiro campo de treinamento, serve para <u>unir</u> mais a comunidade, conhecer mais o Slackware, novas tecnologias e claro, ajudar no desenvolvimento do projeto:
-            </p>
-            <ul>
-                <li>Divulgação</li>
-                <li>Publicidade</li>
-                <li>Ideias</li>
-                <li>Reportar Bug</li>
-                <li>Documentação</li>
-                <li>Mantenedor Slackbuilds</li>
-                <li>Comercial</li>
-                <li>Formas de doação para o projeto Slackware</li>
-                <li>Empacotamento para comunidade</li>
-                <li>Tecnologias para o mundo GNU/Linux em geral</li>
-            </ul>
-
-            <p>
-                Com isso conseguimos manter uma comunidade ainda mais fiel e semear o conhecimento do projeto para os que não conhecem. Assim conseguimos tornar a comunidade mais forte, sem nenhum tipo de barreiras.
-            </p>
-
-            <p>
-                A ideia inicial é ir variando anualmente os estados do Brasil, ou seja, todo ano o evento é organizado em um estado diferente no Brasil e quem sabe um dia no mundo.<br>
-                A votação das cidades é feita pela comunidade, qualquer pessoa pode votar.
-            </p>
-
-            <p>
-
-            </p>
-
-            <p>
-                O evento inicialmente não é endossado pelo projeto Slackware, como dito é um esforço da comunidade, não somos também uma empresa. Tudo é feito de uma forma colaborativa pela comunidade. <b>SEM FINS LUCRATIVOS</b>.
-            </p>
-
-            <h2>O que irei encontrar na SlackCON?</h2>
-            <li>Palestras</li>
-            <li>Oficinas</li>
-            <li>GPG Fest</li>
-            <li>Install Fest</li>
-            <li>Lan OLD Games</li>
-
-            <h2>Quer ajudar o evento?</h2>
-            <p>Acesse <a href="colabore.html">Como colaborar</a> para saber as várias maneiras de colaborar.</p>
-
-        </div> <!--div principal-->
-
-        <!-- Logos dos patricinadores -->
-        <div class="sponsors">
-
-            <h3>Patrocinador Ouro</h3>
-            <img class="sponsorslogo" src="img/goldsponsors.png">
-            <img class="sponsorslogo" src="img/goldsponsors.png">
-
-            <h3>Patrocinador Prata</h3>
-            <img class="sponsorslogo" src="img/silversponsors.png">
-            <img class="sponsorslogo" src="img/silversponsors.png">
-            <img class="sponsorslogo" src="img/silversponsors.png">
-            <img class="sponsorslogo" src="img/silversponsors.png">
-
-            <h3>Patrocinador Bronze</h3>
-
-            <img class="sponsorslogo" src="img/bronzesponsors.png">
-            <img class="sponsorslogo" src="img/bronzesponsors.png">
-            <img class="sponsorslogo" src="img/bronzesponsors.png">
-            <img class="sponsorslogo" src="img/bronzesponsors.png">
-
-            <h3>Parceiros</h3>
-            <a href="https://slackjeff.com.br" target="_blank">Slackjeff</a>
         </div>
-
         <?php require_once 'html/footer.php' ?>
     </body>
 </html>
