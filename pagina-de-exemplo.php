4772d8b075e4a33493463c612c2295a12b575720
viniciusalopes <suporte@viniciusalopes.com.br>
2019-08-10 22:18:31 -0300

    Página de exemplo para criação de novos conteúdos no site.

---
+++ pagina-de-exemplo.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * GNU GENERAL PUBLIC LICENSE -  Version 3, 29 June 2007
+ * < https://www.gnu.org/licenses/gpl-3.0.en.html >  * 
+ * Autor: Jefferson Rocha <root@slackjeff.com.br> 
+ * Data : 2019-08-10
+ */
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <?php require_once 'html/head.php' ?>
+    </head>
+    <body>
+        <header>
+            <?php require_once 'html/botoes-lang.php' ?>
+            <?php require_once 'html/logo.php' ?>
+        </header>
+        <?php require_once 'html/nav.php' ?>
+        <?php require_once 'contents/pagina-de-exemplo-content.php' ?>
+        <?php require_once 'html/footer.php' ?>
+    </body>
+</html>
