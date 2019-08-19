c98c5102dbdd112c867846bdf901c70d72fb5dee
Jefferson Rocha <root@slackjeff.com.br>
2019-08-19 14:19:32 -0300

    up

---
+++ contents/logado-content.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * GNU GENERAL PUBLIC LICENSE -  Version 3, 29 June 2007
+ * < https://www.gnu.org/licenses/gpl-3.0.en.html >  * 
+ * Autor: Jefferson Rocha <root@slackjeff.com.br> 
+ * Data : 2019-08-10
+ */
+?>
+<div id="container">
+    <h1>Bem vindo $USUARIO</h1>
+
+
+<p>
+Para inscrever a sua oficina ou palestra clique nos links abaixos:
+</p>
+
+<div id="menu_logado">
+    <a href="inscricao-oficina.php" class="link_logado">Inscrever Oficina</a>
+    <a href="inscricao-palestra.php" class="link_logado">Inscrever Palestra</a>
+</div>
+
+<p>
+Obrigado por fazer parte da Slackcon.
+</p>
+
+</div>
